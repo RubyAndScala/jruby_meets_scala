064c86f0822d030014a47f7781cfc4c770292338
Martin Mauch <mauch@crealytics.de>
2013-08-10 23:14:56 +0200

    BestCombinationFinder#combinations returns a Combinations object.

--- lib/best_combination_finder.rb
+++ lib/best_combination_finder.rb
@@ -5,9 +5,41 @@ class BestCombinationFinder
   end
 
   def combinations(elements)
-    return [] if elements.empty?
-    first, *rest = elements
-    product = first.product(*rest)
-    product.max_by(&quality_measure)
+    Combinations.new(quality_measure, elements)
+  end
+
+  class Combinations
+    include Enumerable
+    attr_reader :quality_measure, :elements
+    def initialize(quality_measure, elements)
+      @quality_measure = quality_measure
+      @elements = elements
+    end
+
+    def each(*args, &blk)
+      combinations = if elements.empty?
+        [[]]
+      else
+        first, *rest = elements
+        first.product(*rest)
+      end
+      combinations.each(*args, &blk)
+    end
+
+    def all
+      to_a
+    end
+
+    def best
+      max_by(&quality_measure)
+    end
+
+    def eql?(other)
+      to_a.eql?(other)
+    end
+
+    def ==(other)
+      to_a == other
+    end
   end
 end
--- spec/best_combination_finder_spec.rb
+++ spec/best_combination_finder_spec.rb
@@ -7,22 +7,23 @@ describe "BestCombinationFinder" do
   it "should be instantiable" do
     expect { subject }.to_not raise_exception
   end
-  describe "#find_combinations" do
+  describe "#combinations(elements)" do
     subject(:combinations) { finder.combinations(elements) }
     context "for empty elements" do
       let(:elements) { [] }
-      it { should == [] }
+      it { combinations.all.should == [[]] }
+      it { combinations.best.should == []  }
     end
     context "for a single level of elements" do
-      let(:elements) { [[1,2,3]] }
+      let(:elements) { [[1,2]] }
       it "should return the best element according to the quality" do
-        should == [3]
+        combinations.best.should == [2]
       end
     end
     context "for a multiple level of elements" do
-      let(:elements) { [[1,2,3],[4,5,6]] }
+      let(:elements) { [[1,2],[3,4]] }
       it "should return the best element according to the quality" do
-        should == [3,6]
+        combinations.best.should == [2,4]
       end
     end
 
