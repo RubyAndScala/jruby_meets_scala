2248cead3b36e3207ccc38874a90021a32eda6ab
Martin Mauch <mauch@crealytics.de>
2013-08-10 23:16:08 +0200

    Switch to Scala implementation.

--- spec/best_combination_finder_spec.rb
+++ spec/best_combination_finder_spec.rb
@@ -2,24 +2,29 @@ require "spec_helper"
 
 
 describe "BestCombinationFinder" do
-  let(:quality_measure) { lambda {|arr| arr.inject(0, :+) }}
-  subject(:finder) { BestCombinationFinder.new(quality_measure) }
+  let(:quality_measure) { lambda {|arr| arr.inject(0, :+).to_f }}
+  subject(:finder) { Java::OrgAlgorithmsFast::BestCombinationFinder.new(quality_measure) }
+
   it "should be instantiable" do
     expect { subject }.to_not raise_exception
   end
+
   describe "#combinations(elements)" do
     subject(:combinations) { finder.combinations(elements) }
+
     context "for empty elements" do
       let(:elements) { [] }
-      it { combinations.all.should == [[]] }
-      it { combinations.best.should == []  }
+      its(:all) { should == [[]] }
+      its(:best) { should == []  }
     end
+
     context "for a single level of elements" do
       let(:elements) { [[1,2]] }
       it "should return the best element according to the quality" do
         combinations.best.should == [2]
       end
     end
+
     context "for a multiple level of elements" do
       let(:elements) { [[1,2],[3,4]] }
       it "should return the best element according to the quality" do
